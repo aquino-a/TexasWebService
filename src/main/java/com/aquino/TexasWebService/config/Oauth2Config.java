495f437ec548c278d57528aa6970f58140141133
b005 <b005@DESKTOP-RSDBB8J>
2018-06-18 13:13:23 +0900

    added database token store

--- src/main/java/com/aquino/TexasWebService/config/Oauth2Config.java
+++ src/main/java/com/aquino/TexasWebService/config/Oauth2Config.java
@@ -5,6 +5,7 @@
  */
 package com.aquino.TexasWebService.config;
 
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,8 +15,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 /**
  *
@@ -25,49 +27,57 @@ import org.springframework.security.web.authentication.LoginUrlAuthenticationEnt
 @EnableAuthorizationServer
 public class Oauth2Config extends AuthorizationServerConfigurerAdapter {
 
-        @Bean
-        public JwtAccessTokenConverter accessTokenConverter() {
-                return new JwtAccessTokenConverter();
-        }
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        return new JwtAccessTokenConverter();
+    }
 
-        @Autowired
-        private AuthenticationManager authenticationManager;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-        @Override
-        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-                endpoints.authenticationManager(authenticationManager)
-                        .accessTokenConverter(accessTokenConverter());
-        }
+    @Autowired
+    private DataSource dataSource;
 
-        @Override
-        public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-                security.checkTokenAccess("isAuthenticated()");
-        }
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
 
-        @Override
-        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-                // @formatter:off
-                clients.inMemory()
-                                .withClient("my-trusted-client")
-                                .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
-                                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT").scopes("read", "write", "trust")
-                                .resourceIds("oauth2-resource").accessTokenValiditySeconds(600)
-                        .and()
-                                .withClient("my-client-with-registered-redirect").authorizedGrantTypes("authorization_code")
-                                .authorities("ROLE_CLIENT").scopes("read", "trust").resourceIds("oauth2-resource")
-                                .redirectUris("http://anywhere?key=value")
-                        .and()
-                                .withClient("my-client-with-secret")
-                                .authorizedGrantTypes("client_credentials", "password").authorities("ROLE_CLIENT").scopes("read")
-                                .resourceIds("oauth2-resource").secret("{noop}secret")
-                        .and()
-                                .withClient("acme")
-                                .authorizedGrantTypes("authorization_code","password")
-                                .secret("{noop}secret")
-                                .scopes("read","write")
-                                .autoApprove(".*");
-                // @formatter:on
-        }
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter());
+    }
 
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        // @formatter:off
+        clients.inMemory()
+                .withClient("my-trusted-client")
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT").scopes("read", "write", "trust")
+                .resourceIds("oauth2-resource").accessTokenValiditySeconds(600)
+                .and()
+                .withClient("my-client-with-registered-redirect").authorizedGrantTypes("authorization_code")
+                .authorities("ROLE_CLIENT").scopes("read", "trust").resourceIds("oauth2-resource")
+                .redirectUris("http://anywhere?key=value")
+                .and()
+                .withClient("my-client-with-secret")
+                .authorizedGrantTypes("client_credentials", "password").authorities("ROLE_CLIENT").scopes("read")
+                .resourceIds("oauth2-resource").secret("{noop}secret")
+                .and()
+                .withClient("acme")
+                .authorizedGrantTypes("authorization_code", "password")
+                .secret("{noop}secret")
+                .scopes("read", "write")
+                .autoApprove(".*");
+        // @formatter:on
+    }
 
 }
