6d94777fffc086fcc8b8e7117957730ce6e01749
b005 <b005@DESKTOP-RSDBB8J>
2018-06-21 09:33:50 +0900

    take passwords out of main repo

--- src/main/java/com/aquino/TexasWebService/TexasWebServiceApplication.java
+++ src/main/java/com/aquino/TexasWebService/TexasWebServiceApplication.java
@@ -1,18 +1,11 @@
 package com.aquino.TexasWebService;
 
-import java.security.Principal;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableResourceServer
@@ -20,15 +13,10 @@ public class TexasWebServiceApplication {
     
 
 	public static void main(String[] args) {
-		SpringApplication.run(TexasWebServiceApplication.class, args);
+            new SpringApplicationBuilder(TexasWebServiceApplication.class)
+				.properties("spring.config.name:application,config")
+				.build().run(args);
 	}
-        
-//        @RequestMapping("/me")
-//        public Map<String, String> user(Principal principal) {
-//            Map<String, String> map = new LinkedHashMap<>();
-//            map.put("name", principal.getName());
-//            return map;
-//        }
         
         @Configuration
 	protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
